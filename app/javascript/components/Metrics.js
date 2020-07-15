a08e6155279f69539165843d7b0f6bcca724c3a8
Jeffrey Igims <jigims@andrew.cmu.edu>
2020-07-15 15:50:15 -0400

    Added pivot table style

--- app/javascript/components/Metrics.js
+++ app/javascript/components/Metrics.js
@@ -1,15 +1,11 @@
 import React from "react";
 import PivotTableUI from "react-pivottable/PivotTableUI";
-import "react-pivottable/pivottable.css";
 import TableRenderers from "react-pivottable/TableRenderers";
 import Plot from "react-plotly.js";
 import createPlotlyRenderers from "react-pivottable/PlotlyRenderers";
 
 // create Plotly renderers via dependency injection
 const PlotlyRenderers = createPlotlyRenderers(Plot);
-
-// see documentation for supported input formats
-const data = [['attribute', 'attribute2'], ['value1', 'value2']];
 
 class Metrics extends React.Component {
   constructor(props) {
